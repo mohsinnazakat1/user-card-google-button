64acc0f06339370b923bfa335457b4ff52c55bd5
mohsinnazakat1 <csworld7177@gmail.com>
2025-07-24 16:15:04 +0500

    added dynamic url

--- assets/javascripts/discourse/connectors/user-card-additional-buttons/google-button.js
+++ assets/javascripts/discourse/connectors/user-card-additional-buttons/google-button.js
@@ -1,14 +1,31 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class extends Component {
+  @service currentUser;
+
+  get loggedInUsername() {
+    return this.currentUser?.username;
+  }
+
+  get targetUsername() {
+    return this.args.user?.username;
+  }
+
   @action
   goToGoogle() {
-    window.open("https://www.google.com", "_blank", "noopener");
+    if (!this.loggedInUsername || !this.targetUsername) return;
+
+    const url = `https://www.google.com/current_user/${this.loggedInUsername}/target_user/${this.targetUsername}`;
+    window.open(url, "_blank", "noopener");
   }
 
   @action
   goToTwitter() {
-    window.open("https://www.twitter.com", "_blank", "noopener");
+    if (!this.loggedInUsername || !this.targetUsername) return;
+
+    const url = `https://www.twitter.com/current_user/${this.loggedInUsername}/target_user/${this.targetUsername}`;
+    window.open(url, "_blank", "noopener");
   }
 }
