4820cfcc7569ab674598f12f51516f00557d4c2d
mohsinnazakat1 <csworld7177@gmail.com>
2025-07-24 15:31:34 +0500

    added two buttons

--- assets/javascripts/discourse/connectors/user-card-additional-buttons/google-button.js
+++ assets/javascripts/discourse/connectors/user-card-additional-buttons/google-button.js
@@ -6,4 +6,9 @@ export default class extends Component {
   goToGoogle() {
     window.open("https://www.google.com", "_blank", "noopener");
   }
+
+  @action
+  goToTwitter() {
+    window.open("https://www.twitter.com", "_blank", "noopener");
+  }
 }
